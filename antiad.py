fa040e02996982db38fce1950205039290f9dab5
Bail(OnePlus PJF110) <2915289604@qq.com>
2024-08-20 10:01:53 +0800

    增加了手动触发功能 用法: @机器人，空格，跟上广告者qq号

--- antiad.py
+++ antiad.py
@@ -1,34 +1,51 @@
 #Copyright Bail 2024
-#antiad 反广告联合机器人 v1.0.1_2
+#antiad 反广告联合机器人 v1.1_3
 #2024.8.19-2024.8.20
 
-GROUPS = (434025815,)   # 要管理的群列表
+GROUPS = ()   # 要管理的群列表
+NOOPERATORS = ()    # 屏蔽者，也就是滥用手动移除的人
 DATA = '/home/bail/git/qqbot/data'  # 机器人数据目录
 
 import api_ws as api
-import threading,json
+import threading,json,os
 
 ws = api.connect()
-print('I; 已连接')
+print('I: 已连接')
 
 def recv():
     while True:
         msg:dict = json.loads(ws.recv())
         posttype = msg.get('post_type')
-        reqtype = msg.get('request_type')
-        subtype = msg.get('subtype')
-        gid = msg.get('group_id')
+        # 主动检测：被添加或被拉群
         if posttype == 'request':
+            reqtype = msg.get('request_type')
+            subtype = msg.get('subtype')
+            gid = msg.get('group_id')
             if reqtype == 'friend':
                 print('检测到加人广告')
-                blacklist(msg.get('user_id'),msg)
+                aderqq = msg.get('user_id')
+                blacklist(aderqq,f'检测到加人广告: [CQ:at,qq={aderqq}]')
             elif (reqtype == 'group') and (subtype == 'invite') and (gid not in GROUPS):
                 print('检测到拉人广告')
-                blacklist(msg.get('user_id'),msg)
+                aderqq = msg.get('user_id')
+                blacklist(aderqq,f'检测到拉人广告: [CQ:at,qq={aderqq}]')
+        # 被动触发：管理群内被非屏蔽人员@并附带广告人员qq号
+        if posttype == 'message':
+            msgtype = msg.get('message_type')
+            uid = msg.get('user_id')
+            gid = msg.get('group_id')
+            text = msg.get('raw_message')
+            msgid = msg.get('message_id')
+            if (msgtype == 'group') \
+              and (gid in GROUPS) \
+              and (f'[CQ:at,qq={msg.get("self_id")}' in text) \
+              and (uid not in NOOPERATORS):  # 在管理群内被@且发送者未被屏蔽
+                aderqq = int(text.split()[1])
+                blacklist(aderqq,f'[CQ:reply,id={msgid}] 已移除[CQ:at,qq={aderqq}]')
+        print(msg)
 def blacklist(qq:int,msg):
     for i in GROUPS:
-        api.sendg(ws,i,f'检测到广告: [CQ:at,qq={qq}]')
-        print(msg)
+        api.sendg(ws,i,msg)
         api.kick(ws,i,qq,False)
 def main():
     recv()
