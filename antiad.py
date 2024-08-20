dc6e76530e66434f28c1c055d3f4abb284d636ed
Bail(OnePlus PJF110) <2915289604@qq.com>
2024-08-20 22:52:25 +0800

    antiad.py 修改了手动移除逻辑，只有管理人员才能使用

--- antiad.py
+++ antiad.py
@@ -29,23 +29,34 @@ def recv():
                 print('检测到拉人广告')
                 aderqq = msg.get('user_id')
                 blacklist(aderqq,f'检测到拉人广告: [CQ:at,qq={aderqq}]')
-        # 被动触发：管理群内被非屏蔽人员@并附带广告人员qq号
+        # 被动触发：管理群内被管理人员@并附带广告人员qq号
         if posttype == 'message':
             msgtype = msg.get('message_type')
             uid = msg.get('user_id')
             gid = msg.get('group_id')
             text = msg.get('raw_message')
             msgid = msg.get('message_id')
+            sender_role = msg.get('sender').get('role')
             if (msgtype == 'group') \
               and (gid in GROUPS) \
-              and (f'[CQ:at,qq={msg.get("self_id")}' in text) \
-              and (uid not in NOOPERATORS):  # 在管理群内被@且发送者未被屏蔽
-                aderqq = int(text.split()[1])
-                blacklist(aderqq,f'[CQ:reply,id={msgid}] 已移除[CQ:at,qq={aderqq}]')
+              and (f'[CQ:at,qq={msg.get("self_id")}' in text):  # 在管理群内被@
+                msgcut = text.split()
+                if msgcut[1] == 'kick': # kick指令
+                    if sender_role in ('owner','admin'):    # 如果是管理层则执行操作
+                        aderqq = int(msgcut[2])
+                        blacklist(aderqq)
+                    else:   # 如果是普通成员
+                        api.sendg(ws,gid,f'[CQ:reply,id={msgid}] Permission denied')
+                elif msgcut[1] == 'sudo' and msgcut[2] == 'kick':   # 彩蛋: sudo kick指令
+                    if sender_role in ('owner','admin'):    # 如果是管理层则执行操作
+                        aderqq = int(msgcut[2])
+                        blacklist(aderqq)
+                    else:
+                        api.sendg(ws,gid,f'[CQ:reply,id={msgid}] [CQ:at,qq={uid}] is not in the sudoers file. This incident won\'t be reported.')
         print(msg)
 def blacklist(qq:int,msg):
     for i in GROUPS:
-        api.sendg(ws,i,msg)
+        api.sendg(ws,i,f'已移除[CQ:at,qq={qq}]')
         api.kick(ws,i,qq,False)
 def main():
     recv()
