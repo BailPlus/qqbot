458134d61b257337e12f8a5d5c8c1efe32ba13bf
Bail <2915289604@qq.com>
2024-12-24 09:24:30 +0800

    修改了掉线处理逻辑

--- redrock_ctf_monitor.py
+++ redrock_ctf_monitor.py
@@ -9,18 +9,17 @@ USERS = ()
 GROUPS = ()
 HELLO_WORD = '您的好友 红岩ctf看板器 已上线'
 
-import requests,api_ws,time
+import requests,api_ws,time,traceback
 
 class Info:
     def __init__(self,data:dict):
         self.raw_data = data
         self.challenge_cound = data['challengeCount']
 class Monitor:
-    functions = []
     def __init__(self,cookie:str,sleep_time:int|float=10):
         self.cookie = cookie
         self.sleep_time = sleep_time
-        self.functions.extend((self.monitor_new_challenge,))
+        self.functions = (self.monitor_new_challenge,)
         self.ws = api_ws.connect()
         self.push(HELLO_WORD)
     def push(self,msg):
@@ -29,12 +28,14 @@ class Monitor:
             api_ws.sendu(self.ws,i,msg)
         for i in GROUPS:
             api_ws.sendg(self.ws,i,msg)
-    def getinfo(self)->Info:
+    def getinfo(self)->Info|None:
         headers = {'User-Agent':UA,'Cookie':self.cookie}
         try:
             resp = requests.get(URL,headers=headers)
-        except requests.exceptions.ConnectionError:
+        except Exception:
             print('日常掉线，已重新上线')
+            traceback.print_exc()
+            return
         # 请求错误处理
         if resp.status_code != 200:
             self.push(f'请求失败({resp.status_code}): {resp.json()}')
@@ -47,11 +48,13 @@ class Monitor:
     def run(self):
         old_info = self.getinfo()
         while True:
+            time.sleep(self.sleep_time)
             new_info = self.getinfo()
+            if not new_info:
+                continue
             for i in self.functions:
                 i(old_info,new_info)
             old_info = new_info
-            time.sleep(self.sleep_time)
 
 if __name__ == '__main__':
     Monitor(COOKIE).run()
