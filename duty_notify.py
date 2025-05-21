06562852b861b3ec1bef8684cbc2d13843cb090d
Bail <2915289604@qq.com>
2025-05-21 22:51:05 +0800

    宿舍值日题型增加--now模式并优化代码结构

--- duty_notify.py
+++ duty_notify.py
@@ -6,34 +6,44 @@ GID = 0
 DUTIERS = ()
 OFFSET = 0
 
-import api_http as api,time
+import api_http as api,time,sys
 
 def getdutier():
     datestamp = (time.time()+28800)//86400
     index = int((datestamp+OFFSET) % len(DUTIERS))
     return DUTIERS[index]
 
-while True:
-    while True:
-        h,m = map(int,time.strftime('%H:%M').split(':'))
-        print(f'{h}:{m}')
-        if h == 6 and m == 0:
-            print('Time is up!')
-            break
-        elif h == 5:
-            if 0<=m<50:
-                print('sleep 5min')
-                time.sleep(300)
-            elif 50<=m<58:
-                print('sleep 1min')
-                time.sleep(60)
-            else:
-                print('sleep 1s')
-                time.sleep(1)
-        else:
-            print('sleep 1h')
-            time.sleep(3600)
+def push_dutier():
     dutier = getdutier()
-##    print(dutier)
+    ##    print(dutier)
     api.sendg(GID,f'今天轮[CQ:at,qq={dutier}]值日')
-    time.sleep(300)
+
+def main():
+    while True:
+        if '--now' in sys.argv:
+            push_dutier()
+            exit()
+        while True:
+            h,m = map(int,time.strftime('%H:%M').split(':'))
+            print(f'{h}:{m}')
+            if h == 6 and m == 0:
+                print('Time is up!')
+                break
+            elif h == 5:
+                if 0<=m<50:
+                    print('sleep 5min')
+                    time.sleep(300)
+                elif 50<=m<58:
+                    print('sleep 1min')
+                    time.sleep(60)
+                else:
+                    print('sleep 1s')
+                    time.sleep(1)
+            else:
+                print('sleep 1h')
+                time.sleep(3600)
+        push_dutier()
+        time.sleep(300)
+
+if __name__ == '__main__':
+    main()
