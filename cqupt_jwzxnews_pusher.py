ba61235bc9ee63568597c63b435194f611920ea3
Bail <2915289604@qq.com>
2025-07-24 11:15:22 +0800

    教务通知 - 抽离问候语获取器接口 - 适配新网址

--- cqupt_jwzxnews_pusher.py
+++ cqupt_jwzxnews_pusher.py
@@ -11,13 +11,14 @@ import api_http,requests,sys,time,json,os
 
 SUBSCRIBED_USERS = ()   # 已订阅通知的个人
 SUBSCRIBED_GROUPS = ()    # 已订阅通知的群组
-NEWS_URL = 'https://jwc.cqupt.edu.cn/tzgg.htm'  # 通知获取链接
+NEWS_URL = 'https://jw.cqupt.edu.cn/tzgg.htm'  # 通知获取链接
 AI_URL = 'http://localhost:11434/api/generate'  # AI生成api
 UA = 'BailQqbot/0'
 NEWS_FILE = 'news.json'  # 通知列表文件
 FIREFOX_DRIVER_PATH = 'geckodriver'
 
 
+# ===== 数据模型 =====
 @dataclass(frozen=True)
 class News:
     '''教务在线通知类'''
@@ -58,6 +59,7 @@ class News:
         return hash(self.id)
 
 
+# ===== 接口定义 =====
 class NewsGetter(ABC):
     """新闻获取器"""
     @abstractmethod
@@ -65,6 +67,14 @@ class NewsGetter(ABC):
         """获取新闻列表"""
 
 
+class GreetingGetter(ABC):
+    """问候语获取器"""
+    @abstractmethod
+    def get(self) -> str:
+        """获取问候语"""
+
+
+# ===== 接口实现 =====
 class JwzxNewsGetter(NewsGetter):
     """旧版教务在线新闻获取器"""
     def get_news(self) -> list[News]:
@@ -107,17 +117,34 @@ class JwcNewsGetter(NewsGetter):
         self._driver.quit()
 
 
+class RequestsAiGreetingGetter(GreetingGetter):
+    """通过调用本地AI获取早安问候语"""
+    def get(self) -> str:
+        return requests.post(AI_URL, json={
+            "stream": False,
+            "model": "qwen2.5",
+            "prompt": "请使用一句话向同学们问早安，50字左右"
+        })\
+            .json()\
+            .get('response','早上好喵~愿这一天充满平安喜乐！')
+
+
+# ===== 主业务逻辑 =====
 class Bot:
     islazy:bool
     news_getter:NewsGetter
+    greeting_getter:GreetingGetter
 
     def __init__(self,
                  islazy:bool,
-                 news_getter:NewsGetter):
+                 news_getter:NewsGetter,
+                 greeting_getter:GreetingGetter):
         self.islazy = islazy
         self.news_getter = news_getter
+        self.greeting_getter = greeting_getter
+
+        # 如果没有通知列表文件，则创建一个空的通知列表
         if not os.path.exists(NEWS_FILE):
-            # 如果没有通知列表文件，则创建一个空的通知列表
             with open(NEWS_FILE,'w',encoding='utf-8') as f:
                 f.write('[]')
 
@@ -151,44 +178,43 @@ class Bot:
         self.news = news
         return new_news
 
-    def get_goodmorning_words(self)->str:
-        '''通过调用本地AI获取早安问候语'''
-        # TODO: 改成使用ollama sdk进行调用
-        payload = {"stream":False,"model": "qwen2.5","prompt": "请使用一句话向同学们问早安，50字左右"}
-        words = requests.post(AI_URL,json=payload).json().get('response','早上好喵~愿这一天充满平安喜乐！')
-        return words
-
     def construct_post_text(self,news:list[News]):
         '''构造推文'''
         texts = []
         texts.append(time.strftime('今天是%m.%d'))
-        texts.append(self.get_goodmorning_words())
+        texts.append(self.greeting_getter.get())
         if news:
             texts.append('最新教务通知：')
             for i,j in enumerate(news):
-                texts.append(f'{i+1}. {j.title} ( https://jwc.cqupt.edu.cn/{j.id} )')
+                texts.append(f'{i+1}. {j.title} ( https://jw.cqupt.edu.cn/{j.id} )')
         else:
             texts.append('教务在线没有新通知哦~')
         return '\n'.join(texts)
 
     def publish_new_news(self,msg:str):
         '''向订阅用户和群组推送推文'''
-        for i in SUBSCRIBED_USERS:
-            api_http.sendu(i,msg)
-        for i in SUBSCRIBED_GROUPS:
-            api_http.sendg(i,msg)
+        for user in SUBSCRIBED_USERS:
+            api_http.sendu(user,msg)
+        for group in SUBSCRIBED_GROUPS:
+            api_http.sendg(group,msg)
 
+
+# ===== 异常定义 =====
 class EmptyNewsError(ValueError):
     '''获取教务在线通知为空'''
-    def __init__(self,t:str):
+    def __init__(self, t:str):
         self.t = t
     def __str__(self):
         return self.t
 
+
+# ===== 主函数 =====
 def main():
-    islazy = '--lazy' in sys.argv
-    news_getter = JwcNewsGetter()
-    bot = Bot(islazy, news_getter)
+    bot = Bot(
+        islazy='--lazy' in sys.argv,
+        news_getter=JwcNewsGetter(),
+        greeting_getter=RequestsAiGreetingGetter()
+    )
     isnow = '--now' in sys.argv
     if isnow:
         new_news = bot.get_new_news()
@@ -204,6 +230,7 @@ def main():
             msg = bot.construct_post_text(new_news)
             bot.publish_new_news(msg)
         time.sleep(3600)
+
 
 if __name__ == '__main__':
     sys.exit(main())
